2ec88154f05c7415434df71ea58983956530b796
Erisia Secundus <minecraft@node02.atelieraphelion.com>
2020-04-23 14:04:01 -0700

    Just general updates

--- base/server/start.sh
+++ base/server/start.sh
@@ -75,12 +75,12 @@ dailyRestart() {
     set +x
     while true; do
       sleep 45
-      if [[ $(date +%R) = 06:00 ]]; then
+      if [[ $(TZ=Europe/Dublin date +%R) = 10:00 ]]; then
         set -x
         ./stop.sh
         exit
       fi
-      if [[ $(date +%R) = 18:00 ]]; then
+      if [[ $(TZ=Europe/Dublin date +%R) = 20:00 ]]; then
         set -x
         ./stop.sh
         exit
