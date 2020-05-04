7a6d4b05545ec83e7f917563491819310afeea6d
Erisia Secundus <minecraft@node02.atelieraphelion.com>
2020-05-04 13:26:05 -0700

    Update nixpkgs

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,9 +1,9 @@
 import (builtins.fetchTarball {
   # Descriptive name to make the store path easier to identify
   name = "nixos-minecraft";
-  # Commit hash for nixos-20.03 as of 2020-03-19
+  # Commit hash for nixos-20.03 as of 2020-05-03
   # Obtained from https://status.nixos.org/
-  url = https://github.com/nixos/nixpkgs/archive/b2935fbeceaea0b64df4401545d7c8ea29102120.tar.gz;
+  url = https://github.com/nixos/nixpkgs/archive/ab3adfe1c769c22b6629e59ea0ef88ec8ee4563f.tar.gz;
   # Hash obtained using `nix-prefetch-url --unpack <url>`
-  sha256 = "1z1631v7q2c8mavy7xnvfx0wz34zd49jqmjg66nk0qgsi605m3qp";
+  sha256 = "1m4wvrrcvif198ssqbdw897c8h84l0cy7q75lyfzdsz9khm1y2n1";
 })
