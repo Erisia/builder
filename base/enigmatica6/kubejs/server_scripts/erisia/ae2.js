6838c2595634b5d6c4b86122e239fe0643fac6e3
TheCowKing <derrekschuurman@gmail.com>
2021-08-09 07:06:24 -0700

    AE2 Scripts (#248)
    * Delete IF pity machine frame expert recipe
    
    We are not in expert mode and this recipe being used is a (known, according to the Enigmatica discord) bug.
    
    * old E6 ae2 scripts and conversions
    
    Co-authored-by: Random832 <random832@fastmail.com>
    Co-authored-by: Maxwell-lt <maxwell.lt@live.com>
    Co-authored-by: Derrek <derreksherman@protonmail.com>
    Co-authored-by: Luke Ridge <castone22@gmail.com>

--- base/enigmatica6/kubejs/server_scripts/erisia/ae2.js
+++ base/enigmatica6/kubejs/server_scripts/erisia/ae2.js
@@ -16,6 +16,95 @@ onEvent('recipes', event => {
   })
 })
 
+//Fluix using EE
+events.listen('recipes', function (event) {
+    event.recipes.interactio.item_fluid_transform({
+        inputs: [
+            {
+                tag: 'forge:dusts/redstone',
+                count: 1
+            },
+            {
+                tag: 'forge:gems/charged_certus_quartz',
+                count: 1
+            },
+            {
+                tag: 'forge:gems/quartz',
+                count: 1
+            }
+        ],
+        fluid: {
+            fluid: 'water'
+        },
+        output: {
+            item: 'emendatusenigmatica:gem_fluix',
+            count: 2
+        },
+
+        consume_fluid: 0.0
+    });
+});
+
+//Powah Integration Charging
+events.listen('recipes', function (event) {
+    event.recipes.powah.energizing({
+        ingredients: [{ tag: 'forge:gems/certus_quartz' }],
+        energy: 10000,
+        result: {
+            item: getPreferredItemInTag(ingredient.of('#forge:gems/charged_certus_quartz')).id
+        }
+    });
+    event.recipes.powah.energizing({
+        ingredients: [
+            { tag: 'forge:gems/charged_certus_quartz' },
+            { tag: 'forge:dusts/redstone' },
+            { tag: 'forge:gems/quartz' }
+        ],
+        energy: 4000,
+        result: {
+            item: getPreferredItemInTag(ingredient.of('#forge:gems/fluix')).id,
+            count: 2
+        }
+    });
+});
+
+// adds recipes to switch between ee and ae2 items
+onEvent('recipes', (event) => {
+
+    const recipes = [
+        {
+            output: 'appliedenergistics2:certus_quartz_crystal',
+            inputs: ['emendatusenigmatica:certus_quartz_gem']
+        },
+        {
+            output: 'emendatusenigmatica:certus_quartz_gem',
+            inputs: ['appliedenergistics2:certus_quartz_crystal']
+        },
+        {
+            output: 'emendatusenigmatica:charged_certus_quartz_gem',
+            inputs: ['appliedenergistics2:charged_certus_quartz_crystal']
+        },
+        {
+            output: 'appliedenergistics2:charged_certus_quartz_crystal',
+            inputs: ['emendatusenigmatica:charged_certus_quartz_gem']
+        },
+        {
+            output: 'emendatusenigmatica:fluix_gem',
+            inputs: ['appliedenergistics2:fluix_crystal']
+        },
+        {
+            output: 'appliedenergistics2:fluix_crystal',
+            inputs: ['emendatusenigmatica:fluix_gem']
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        recipe.id
+            ? event.shapeless(recipe.output, recipe.inputs).id(recipe.id)
+            : event.shapeless(recipe.output, recipe.inputs);
+    });
+});
+
 onEvent('item.tags', event => {
   event.add('appliedenergistics2:dusts/ender', 'emendatusenigmatica:ender_dust')
 })
