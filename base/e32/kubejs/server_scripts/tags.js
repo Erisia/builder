aa3fa18368240916fbbf787b4fa8f42950c16e41
Gregory Maddra <bloxgate@gmail.com>
2024-04-02 22:17:39 -0500

    Delete e32 base pack

--- base/e32/kubejs/server_scripts/tags.js
+++
@@ -1,7 +0,0 @@
- 
-ServerEvents.tags('item', event => {
-
-    // silver ore
-    event.get("c:raw_ores").add("galosphere:raw_silver")
-    event.get("c:raw_silver_ores").add("galosphere:raw_silver")
-})
