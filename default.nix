aab4634ca7e263358200ded203a9290f169b2e68
Lucas Ridge <lzridge.04@gmail.com>
2024-08-24 21:20:16 +0000

    add cleanroom support.. maybe

--- default.nix
+++ default.nix
@@ -6,49 +6,25 @@ with callPackage ./lib/lib.nix {};
 rec {
 
   packs = {
-    e33 = buildPack e33;
-    e33_5 = buildPack e33_5;
+    # e33 = buildPack e33;
+    # e33_5 = buildPack e33_5;
+    e34 = buildPack e34;
   };
   
-  e33_5 = {
-    name = "E33.5";
-    tmuxName = "e335";
-    description = "E33.5: Exiled Incognito";
-    ram = "16G";
-    port = 25566;
-    prometheusPort = 1225;
-    minecraft = "1.20.1";
-    forge = {
-      major = "1.20.1";
-      minor = "47.2.18";
-    };
-    extraDirs = [
-      ./base/e33_5
-    ];
-    extraServerDirs = [
-      ./base/e33_5-server
-      ./base/server
-    ];
-    extraClientDirs = [
-      ./base/e33_5-client
-    ];
-    manifest = ./manifest/e33_5.json;
-  };
-
-  e33 = {
-    name = "E33";
-    tmuxName = "e33";
-    description = "E33: Pars Una";
-    ram = "16G";
+  e34 = {
+    name = "E34";
+    tmuxName = "e34";
+    description = "E34: Plerumque Nubila Meatballs";
+    ram = "8G";
     port = 25565;
     prometheusPort = 1224;
-    minecraft = "1.20.1";
-    neoforge = {
-      major = "1.20.1";
-      minor = "47.1.104";
+    minecraft = "1.12.2";
+    cleanroom = {
+      major = "0.2.3";
+      minor = "alpha";
     };
     extraDirs = [
-      ./base/e33
+      ./base/e34
       ./base/erisia
     ];
     extraServerDirs = [
@@ -59,6 +35,56 @@ rec {
     ];
     manifest = ./manifest/e33.json;
   };
+
+  # e33_5 = {
+  #   name = "E33.5";
+  #   tmuxName = "e335";
+  #   description = "E33.5: Exiled Incognito";
+  #   ram = "8G";
+  #   port = 25566;
+  #   prometheusPort = 1225;
+  #   minecraft = "1.20.1";
+  #   forge = {
+  #     major = "1.20.1";
+  #     minor = "47.2.18";
+  #   };
+  #   extraDirs = [
+  #     ./base/e33_5
+  #   ];
+  #   extraServerDirs = [
+  #     ./base/e33_5-server
+  #     ./base/server
+  #   ];
+  #   extraClientDirs = [
+  #     ./base/e33_5-client
+  #   ];
+  #   manifest = ./manifest/e33_5.json;
+  # };
+
+  # e33 = {
+  #   name = "E33";
+  #   tmuxName = "e33";
+  #   description = "E33: Pars Una";
+  #   ram = "16G";
+  #   port = 25565;
+  #   prometheusPort = 1224;
+  #   minecraft = "1.20.1";
+  #   neoforge = {
+  #     major = "1.20.1";
+  #     minor = "47.1.104";
+  #   };
+  #   extraDirs = [
+  #     ./base/e33
+  #     ./base/erisia
+  #   ];
+  #   extraServerDirs = [
+  #     ./base/server
+  #   ];
+  #   extraClientDirs = [
+  #     ./base/client
+  #   ];
+  #   manifest = ./manifest/e33.json;
+  # };
 
   ServerPack = buildServerPack rec {
     inherit packs;
