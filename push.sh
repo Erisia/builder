bb5ddd0b3359e407278999354ba3cf303e07bd13
Erisia Administrator <minecraft@madoka.brage.info>
2025-06-08 00:13:17 +0100

    ..and actually pause before pushing

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "This script will push the currently checked-out (and named) commit as master."
 echo "Press ctrl-c if this isn't intended"
+read
 
 jj b s master -r 'latest(ancestors(@) & ~empty() & ~description(exact:""))'
 jj git push
