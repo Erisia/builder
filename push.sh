4e2a9099a104ef6cdf6d8afe0923b804860ad02a
Erisia Administrator <minecraft@madoka.brage.info>
2025-06-08 00:08:07 +0100

    Add push.sh

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "This script will push the currently checked-out (and named) commit as master."
+echo "Press ctrl-c if this isn't intended"
+
+jj b s master -r 'latest(ancestors(@) & ~empty() & ~description(exact:""))'
+jj git push
