20d5c5589fe68c1c8c7a9591d49d812654d22b61
Erisia Administrator <minecraft@madoka.brage.info>
2026-05-07 15:50:29 +0100

    Use system tmux

--- base/server/start.py
+++ base/server/start.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i python3 -p python3 python3Packages.psutil python3Packages.rich rsync tmux systemd
+#!nix-shell -i python3 -p python3 python3Packages.psutil python3Packages.rich rsync systemd
 
 import os
 import sys
